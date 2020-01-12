f420236141f0e1a2b3dd5ab36da0f2cf4bd890d6
Damely Tineo <damely.tineo07@gmail.com>
2020-01-12 00:28:38 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,14 @@
 def square_array(array)
   new_array = [ ]
-  array.each do |numbers|
-    (x = numbers**2)
-    new_array << x
+  array.each do |number|
+    new_array << (number**2)
   end
   return new_array
+end
+
+def squared_array(array)
+  array.map do |number|
+    number**2
+  end
+  return array
 end
